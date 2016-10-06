d731e948aaf9d57a7fcf87fa2b5797bb822578b7
ionakathryn <ionakathryn123@googlemail.com>
2016-10-06 14:05:43 +0100

    Update Assignment1inProgress.java

--- assignments/Assignment1inProgress.java
+++ assignments/Assignment1inProgress.java
@@ -3,16 +3,15 @@ public class Assignment1 {
         int angle = 15;
         double range;
         int speed;
+        System.out.printf("Speed\t" + "Angle\t" + "Range\n");
         
         for (speed = 80; speed <=140; speed +=20){
-        	System.out.println("");
-            
             for (angle = 15; angle <=75; angle+=15){
             double radians = Math.toRadians(angle);
-            System.out.printf("Speed (m/s) = " + speed + "\t");
-            System.out.printf("Angle (degrees) = " + angle + "\t");
+            System.out.printf(speed + "\t");
+            System.out.printf(angle + "\t");
             range = speed * speed * Math.sin(2 * radians) / 9.8;
-            System.out.printf("Range (m) = " + range +"\n");
+            System.out.printf(range +"\n");
             }
     }
     System.out.println("");
