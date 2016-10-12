98a6227e6d0a43e627445bced04be1b8b6fc2ad1
ionakathryn <ionakathryn123@googlemail.com>
2016-10-12 21:26:42 +0100

    Update Assignment1inProgress.java

--- assignments/Assignment1inProgress.java
+++ assignments/Assignment1inProgress.java
@@ -1,20 +1,31 @@
-public class Assignment1 {
-    public static void main(String[] args) {
+import java.io.PrintStream;
+
+public class Assignment1 
+{
+    public static void main(String[] args) 
+    {
+    	PrintStream output = new PrintStream(System.out);
+
         int angle = 15;
         double range;
         int speed;
-        System.out.printf("Speed\t" + "Angle\t" + "Range\n");
+        System.out.println("******************************************");
+      	output.format("* Speed(m/s) * Angle(degrees) * Range(m)*");
         
-        for (speed = 80; speed <=140; speed +=20){
-            for (angle = 15; angle <=75; angle+=15){
+        System.out.println(" ");
+        
+        for (speed = 80; speed <=140; speed +=20)
+        {
+            for (angle = 15; angle <=75; angle+=15)
+            {
             double radians = Math.toRadians(angle);
-            System.out.printf(speed + "\t");
-            System.out.printf(angle + "\t");
+            output.format("* %8d   *", speed);
+            output.format(" %8d   *", angle);
             range = speed * speed * Math.sin(2 * radians) / 9.8;
-            System.out.printf(range +"\n");
+            output.format(" %8.2f   *\t\n",range);
             }
-    }
-    System.out.println("");
+        }
+    	output.format("");
     }
     
     
