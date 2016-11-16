4055ed1957a73fcfb04dad6a5b8bcce8c6119955
ionakathryn <ionakathryn123@googlemail.com>
2016-11-16 20:51:30 +0000

    Update House.java

--- week5/House.java
+++ week5/House.java
@@ -47,37 +47,66 @@ public class House extends Applet {
 	g.drawLine (200,140,220,140);
 	g.drawLine (200,140, 200, 120);
 	g.drawLine (220,140, 220, 120);
-	g.setColor (Color.red);
+	g.setColor (Color.black);
 	g.fillRect (120, 180, 180, 120);
 
-	// windows and door of house
+	// windows 
 	g.setColor (Color.magenta);
 	g.fillRect (140,200, 40, 20);
 	g.fillRect (240,200, 40, 20);
 	g.fillRect (140,260, 40, 20);
 	g.fillRect (240,260, 40, 20);
 	g.setColor (Color.black);
-	g.drawRect (200,260, 20, 40);
-	g.fillRect (200,260, 20,40);
-    	g.drawLine (200, 220, 200,200);
+
+    g.drawLine (200, 220, 200,200);
 	g.drawLine (200, 200, 220, 200);
 	g.drawLine (200, 220, 220, 220);
 	g.drawLine (220,200, 200, 200);
 	g.drawLine (220, 220, 220, 200);
 
+	//door
+	g.setColor (Color.red);
+	g.fillRect (200,260, 20,40);
+
 	// draw roof
 	g.drawLine (160,140, 120, 180);
 	g.drawLine (260, 140, 300,180);
 	g.drawLine (160, 140, 260, 140);
+	g.setColor (Color. black);
+	g.fillRect (200, 120, 20,20);
+
+	
+
 
 	//sunshine
 	g.setColor (Color.orange);
 	g.fillOval (90, 90, 25, 25);
 	g.drawLine (100,80, 100,130);
 	g.drawLine (80, 100, 130, 100);
+	g.drawLine (80, 80, 140,140);
+	g.drawLine (120, 70, 80, 135);
 
 	//background
 	setBackground (Color.cyan);
+	g.setColor (Color.green);
+	g.fillRect (0, 300, 400, 100);
+
+	//path 
+	g.setColor (Color.white);
+	g.drawLine (180, 300, 180, 400);
+	g.drawLine (240, 300, 240, 400);
+	g.setColor (Color. orange);
+	g.fillRect (204, 300, 20, 100);
+
+	//clouds
+	g.setColor (Color.white);
+	g.fillOval (280, 70, 30,20);
+	g.fillOval (300, 80, 30, 10);
+	g.fillOval (280, 50, 45, 25);
+	g.fillOval (300, 60, 75, 50);
+
+	//fence
+
 
 	}
 
