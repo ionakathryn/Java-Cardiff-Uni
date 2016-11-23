6d24eb699e54e89744a42f4577de95b5bfcb7f49
ionakathryn <ionakathryn123@googlemail.com>
2016-11-23 17:12:54 +0000

    Update House.java

--- week5/House.java
+++ week5/House.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 
 public class House extends Applet {
 	public void paint (Graphics g){
+	
 	g.fillRect (280, 110, 50, 200);
 	g.setColor (Color.white);
 	g.fillRect (280, 120, 10, 10);
@@ -33,9 +34,7 @@ public class House extends Applet {
 	g.fillRect (280, 240, 10, 10);
 	g.fillRect (300, 240, 10, 10);
 	g.fillRect (320, 240, 10, 10);
-
-	//skyscraper door
-	g.fillRect (297, 270, 20, 40);
+	
 
 	// house outline
 	g.drawLine (120, 300, 300, 300);
@@ -50,36 +49,64 @@ public class House extends Applet {
 	g.setColor (Color.black);
 	g.fillRect (120, 180, 180, 120);
 
-	// windows 
-	g.setColor (Color.magenta);
+	g.setColor (Color.white);
 	g.fillRect (140,200, 40, 20);
 	g.fillRect (240,200, 40, 20);
 	g.fillRect (140,260, 40, 20);
 	g.fillRect (240,260, 40, 20);
-	g.setColor (Color.black);
 
-    g.drawLine (200, 220, 200,200);
-	g.drawLine (200, 200, 220, 200);
-	g.drawLine (200, 220, 220, 220);
-	g.drawLine (220,200, 200, 200);
-	g.drawLine (220, 220, 220, 200);
+	g.setColor (Color.magenta);
+	g.fillRect (155, 235, 10,10);
+	g.fillRect (255, 235, 10, 10);
+
+	g.fillRect (200, 200, 20, 20);
+	g.fillRect (205, 230, 10, 10);
+
 
 	//door
 	g.setColor (Color.red);
 	g.fillRect (200,260, 20,40);
+	g.setColor (Color.black);
+	g.fillOval (212, 280, 5, 5);
 
 	// draw roof
 	g.drawLine (160,140, 120, 180);
 	g.drawLine (260, 140, 300,180);
 	g.drawLine (160, 140, 260, 140);
-	g.setColor (Color. black);
-	g.fillRect (200, 120, 20,20);
 
-	
+
+	//hotizontal lines
+	g.drawLine (123, 175, 295, 175);
+	g.drawLine (127, 170, 290, 170);
+	g.drawLine (136, 165, 286, 165);
+	g.drawLine (140, 160, 280, 160);
+	g.drawLine (143, 155, 277, 155);
+	g.drawLine (150, 150, 270, 150);
+	g.drawLine (156, 145, 265, 145);
+
+	g.setColor (Color.magenta);
+	//vertical lines
+	g.drawLine (130, 170, 130, 180);
+	g.drawLine (140, 160, 140, 180);
+	g.drawLine (150, 150, 150, 180);
+	g.drawLine (160, 140, 160, 180);
+	g.drawLine (170, 140, 170, 180);
+	g.drawLine (180, 140, 180, 180);
+	g.drawLine (190, 140, 190, 180);
+	g.drawLine (200, 140, 200, 180);
+	g.drawLine (210, 140, 210, 180);
+	g.drawLine (220, 140, 220, 180);
+	g.drawLine (230, 140, 230, 180);
+	g.drawLine (240, 140, 240, 180);
+	g.drawLine (250, 140, 250, 180);
+	g.drawLine (260, 140, 260, 180);
+	g.drawLine (270, 150, 270, 180);
+	g.drawLine (280, 160, 280, 180);
+	g.drawLine (290, 170, 290, 180);
 
 
 	//sunshine
-	g.setColor (Color.orange);
+	g.setColor (Color.yellow);
 	g.fillOval (90, 90, 25, 25);
 	g.drawLine (100,80, 100,130);
 	g.drawLine (80, 100, 130, 100);
@@ -90,13 +117,14 @@ public class House extends Applet {
 	setBackground (Color.cyan);
 	g.setColor (Color.green);
 	g.fillRect (0, 300, 400, 100);
+	g.setColor (Color.lightGray);
+	g.fillRect (0, 320, 400, 100);
 
 	//path 
-	g.setColor (Color.white);
-	g.drawLine (180, 300, 180, 400);
-	g.drawLine (240, 300, 240, 400);
-	g.setColor (Color. orange);
+	g.setColor (Color. darkGray);
 	g.fillRect (204, 300, 20, 100);
+	g.setColor (Color.yellow);
+	g.fillRect (209, 300, 10, 100);
 
 	//clouds
 	g.setColor (Color.white);
@@ -106,6 +134,37 @@ public class House extends Applet {
 	g.fillOval (300, 60, 75, 50);
 
 	//fence
+	g.setColor (Color.red);
+	g.drawLine (90, 400, 130, 340);
+	g.drawLine (130, 340, 180, 340);
+	g.drawLine (240, 340, 290, 340);
+	g.drawLine (290, 340, 340, 400);
+	g.drawLine (130, 340, 290, 340);
+
+	//smoke
+	g.setColor (Color.orange);
+	g.fillOval (210, 90, 40, 23);
+	g.fillOval (205, 93, 50, 40);
+	g.fillOval (190, 93, 35, 20);
+	g.fillOval (230, 90, 40, 30);
+	g.fillOval (240, 80, 30, 30);
+	
+
+	//chimney
+	g.setColor (Color. black);
+	g.fillRect (200, 120, 20,20);
+
+	//window panes
+	g.drawLine (160, 220, 160, 200);
+	g.drawLine (140, 210, 180, 210);
+	g.drawLine (260, 200, 260, 220);
+	g.drawLine (240, 210, 280, 210);
+	g.drawLine (160, 260, 160, 280);
+	g.drawLine (140, 270, 180, 270);
+	g.drawLine (160, 260, 160, 280);
+	g.drawLine (260, 260, 260, 280);
+	g.drawLine (240, 270, 280, 270);
+
 
 
 	}
