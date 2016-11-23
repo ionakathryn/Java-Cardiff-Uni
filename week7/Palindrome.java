d09d55b26d45f8e7053153a9379a8b1d6c08a1ca
ionakathryn <ionakathryn123@googlemail.com>
2016-11-23 20:17:03 +0000

    Create Palindrome.java

---
+++ week7/Palindrome.java
@@ -0,0 +1,46 @@
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class Palindrome {
+	public static void main(String[] args)
+		throws IOException {
+
+		// user enters string
+		System.out.println ("Please enter string");
+
+		InputStreamReader input = new InputStreamReader (System.in);
+		BufferedReader reader = new BufferedReader (input);
+		String userinput = "";
+		userinput = reader.readLine();
+
+		// test if string is valid
+			if (!userinput.matches("[A-Za-z]+")){
+				System.out.println ("Invalid Input");
+				System.exit(0);
+			}
+
+			else if (userinput.length() > 10){
+				System.out.println ("String too long");
+				System.exit(0);
+			}
+
+
+		String stringReverse = "";
+		for (int i = userinput.length()-1; i >= 0; i--){
+			stringReverse += String.valueOf(userinput.charAt(i));
+		}
+
+		if (userinput.equals(stringReverse)){
+			System.out.println ("String is a Palindrome");
+			System.exit(0);
+		}
+
+		else if (userinput != stringReverse) {
+			System.out.println ("String is not a Palindrome");
+			System.exit(0);
+		}
+
+		}
+
+}
