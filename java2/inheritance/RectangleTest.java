fd961e0cb5ea82db572342a4cc632e12891d8f07
ionakathryn <ionakathryn123@googlemail.com>
2017-01-25 20:22:32 +0000

    Create RectangleTest.java

---
+++ java2/inheritance/RectangleTest.java
@@ -0,0 +1,11 @@
+public class RectangleTest
+{
+	public static void main(String[] args)
+	{
+		Box testBox = new Box(5,5,5);
+		testBox.surfaceArea();
+		testBox.volume();
+		testBox.bodyDiagonalLength();
+
+	}
+}
