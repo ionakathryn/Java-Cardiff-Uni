31bd5d8d488a0002bfda266c3bf65f0192a914ec
ionakathryn <ionakathryn123@googlemail.com>
2017-01-25 20:48:32 +0000

    Create Isosceles.java

---
+++ java2/inheritance/assignment2/Isosceles.java
@@ -0,0 +1,18 @@
+public class Isosceles
+{
+	private double length;
+	public Isosceles(double lengthValue)
+	{
+	length = lengthValue;
+	}
+
+	public double area(double angle)
+	{
+		return 0.5 * length * length * Math.sin(angle * 3.14159 / 180);
+	}
+
+	public double baseLength(double angle)
+	{
+		return 2 * length * Math.sin((angle/2) * 3.14159 / 180);
+	}
+}
