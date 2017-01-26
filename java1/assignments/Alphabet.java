a928416587c3c69e977c3f71bfcae7e5946c6330
ionakathryn <ionakathryn123@googlemail.com>
2017-01-26 10:52:25 +0000

    Create Alphabet.java

---
+++ java1/assignments/Alphabet.java
@@ -0,0 +1,35 @@
+/*
+ * Alphabet.java
+ * 
+ * V001
+ *
+ * Dec 2016
+ * 
+ * Author: Iona-Kathryn Evans
+ */
+
+public class Alphabet {
+
+ public static void main(String[] args) {
+ checkAlphabetic("fdsfsfd432423423!");
+ }
+
+ public static boolean checkAlphabetic(String input) {
+ char[] chars = input.toCharArray();
+ int count = 0;
+
+ for (int i = 0; i < input.length(); i++) {
+ if (Character.isLetter(chars[i])) {
+ count = 1;
+} else {
+
+System.out.println("String is not alphabetic....");
+System.exit(1);
+return false;
+}
+}
+System.out.println("alphabetic word");
+
+return true;
+  }
+}
