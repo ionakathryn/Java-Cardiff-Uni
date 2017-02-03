0dcbb601295c46bae72fb402e434dd508a0ec5ca
ionakathryn <ionakathryn123@googlemail.com>
2017-02-03 15:17:41 +0000

    Create Assignment2MouseListener.java

---
+++ java2/inheritance/GUI/Assignment2/Assignment2MouseListener.java
@@ -0,0 +1,26 @@
+import java.awt.event.MouseListener;
+
+
+public class Assignment2MouseListener implements MouseListener {
+
+    public void mouseClicked(MouseEvent arg0) {
+
+
+    }
+
+    public void mouseExited(MouseEvent arg0) {
+
+    }
+
+    public void mouseEntered(MouseEvent arg0) {
+
+    }
+
+    public void mousePressed(MouseEvent arg0) {
+
+    }
+
+    public void mouseReleased(MouseEvent arg0) {
+
+    }
+}
