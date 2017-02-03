0b0ec39ef2c5b5b54f11eef3be9db6539f01f4da
ionakathryn <ionakathryn123@googlemail.com>
2017-02-03 15:28:13 +0000

    Create Excersise1.java

---
+++ java1/Excerises/Excersise1.java
@@ -0,0 +1,33 @@
+import java.lang.*;
+  import java.io.PrintStream;
+
+
+  public class Excersise1 {
+
+  public static void main (String args[]){
+      PrintStream output = new PrintStream (System.out);
+      System.out.println("   *************************************************");
+
+      output.format("   *  Speed(m/s) *  Angle(degrees)  *   Range(m)   *\n");
+      System.out.println("   *************************************************");
+
+      int speed = 0;
+      int angle = 15;
+      double range;
+      for (int i=0; i <= 1; i++){
+          
+          speed +=10;
+          angle = 15;
+          
+          for (int x=0; x<=2; x++){
+              angle +=15;
+              double radians = Math.toRadians(angle);
+              range = speed * speed * Math.sin(2*radians) / 9.8;
+              output.format ("   * %8d    * %8d         * %8.2f     * \n", speed, angle,range);
+
+          }
+         }
+                 System.out.println("   *************************************************");
+
+  }
+  }
