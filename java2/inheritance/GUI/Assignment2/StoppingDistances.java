80807b10315c4ab8bbe514097fa131e541465918
ionakathryn <ionakathryn123@googlemail.com>
2017-02-17 11:35:06 +0000

    Update StoppingDistances.java

--- java2/inheritance/GUI/Assignment2/StoppingDistances.java
+++ java2/inheritance/GUI/Assignment2/StoppingDistances.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.Panel;
 
 public class StoppingDistances
     {
@@ -20,45 +21,48 @@ public class StoppingDistances
             final int DEFAULT_FRAME_HEIGHT = 250;
             setSize(DEFAULT_FRAME_WIDTH, DEFAULT_FRAME_HEIGHT);
 
-            // set layout
-            GridLayout layout = new GridLayout( 5, 2);
-            setLayout(layout);
-            add(new Label("Start"));
-
             // create two new panels
             panel1 = new Panel();
-            panel2 = new Panel();
+            panel1.setLayout(new GridLayout(5,2));
 
-
+            panel1.add(new Label("Start"));
             startSpeed = new TextField();
-            add(startSpeed);
+            panel1.add(startSpeed);
 
 
-            add(new Label("End"));
-
+            panel1.add(new Label("End"));
             endSpeed = new TextField();
-            add(endSpeed);
-            endSpeed.setEditable(true);
+            panel1.add(endSpeed);
 
-            add(new Label ("Increment"));
-
+            panel1.add(new Label ("Increment"));
             incrementSpeed = new TextField();
-            add(incrementSpeed);
-            incrementSpeed.setEditable(true);
+            panel1.add(incrementSpeed);
+
 
             clear = new Button("Clear");
-            add(clear);
+            panel1.add(clear);
             clear.addActionListener(this);
 
             table = new Button("Table");
-            add(table);
+            panel1.add(table);
             table.addActionListener(this);
 
             exit = new Button("Exit");
-            add(exit);
+            panel1.add(exit);
             exit.addActionListener(this);
 
+            panel2 = new Panel();
+            panel2.setLayout(new GridLayout(1,1) );
+            area = new TextArea( "", 0, 0, TextArea.SCROLLBARS_VERTICAL_ONLY );
+            area.setEditable(false);
+            panel2.add(area);
+            setLayout(new GridLayout(2,1));
+
+            add(panel1);
             add(panel2);
+
+
+
         }
 
         public void actionPerformed( ActionEvent e){
@@ -71,9 +75,24 @@ public class StoppingDistances
             else if (e.getSource() == exit) {
                 System.exit(0);
             }
+            else if (e.getSource() == table){
+                start = Double.parseDouble(startSpeed.getText());
+                end = Double.parseDouble(endSpeed.getText());
+                increment = Double.parseDouble(incrementSpeed.getText());
+                for (double i = start; i < end; i =+ increment){
+                    area.setText("Speed = " + i + "\n");
+                }
+
+
+
+
+            }
         }
         private TextField startSpeed,endSpeed,incrementSpeed;
         private Button clear,table,exit;
-        private Panel panel1, panel2;
+        private Panel panel1,panel2;
+        private TextArea area;
+        private double start,end,increment, finalSpeed;
+        private int distance;
     }
 
