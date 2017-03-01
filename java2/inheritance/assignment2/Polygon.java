5772076367261c7cc503fa4dac3c195676af0d73
ionakathryn <ionakathryn123@googlemail.com>
2017-03-01 20:18:50 +0000

    Update Polygon.java

--- java2/inheritance/assignment2/Polygon.java
+++ java2/inheritance/assignment2/Polygon.java
@@ -8,14 +8,12 @@ public class Polygon extends Isoceles
 	public double polygonArea()
 	{
 		double polygonarea = area(360/sides) * sides;
-		System.out.println(polygonarea);
 		return polygonarea;
 	}
 	public double polygonPerimeter()
 	{
 		double perimeter = baseLength(360/sides) * sides;
-		System.out.println(perimeter);
 		return perimeter;
 	}
-private double sides;
+	private double sides;
 }
