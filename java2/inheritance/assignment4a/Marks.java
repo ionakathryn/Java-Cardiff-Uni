d864619b4cd5f2acfa5140de1a53bbd1fe707e92
ionakathryn <ionakathryn123@googlemail.com>
2017-03-05 19:24:54 +0000

    Update Marks.java

--- java2/inheritance/assignment4a/Marks.java
+++ java2/inheritance/assignment4a/Marks.java
@@ -9,14 +9,15 @@ public class Marks {
         String output = "";
         String[] arr = new String[60];
         String[] arrSplit = new String[60];
-        String[] sorted = new String[60];
+        String[] sorted = new String[arrSplit.length];
         int i = 0;
+        String delimiters = "[0-9]";
 
 
         try {
             in = new BufferedReader(new FileReader("Marks.txt"));
         } catch (FileNotFoundException e) {
-            System.out.println("File Not Found!!");
+            System.out.println("File Not Found!");
             System.exit(0);
         }
 
@@ -32,32 +33,54 @@ public class Marks {
             }
         }
         
-
+        // split string when int is found
         for (int j = 0; j < arr.length; j++) {
             if (arr[j] != null) {
                 for (int k = 0; k < arr.length; k++) {
-                    arrSplit[j] = (Arrays.toString(arr[j].split(" ")));
+                    arrSplit[j] = (Arrays.toString(arr[j].split(delimiters)));
                 }
 
             }
 
         }
         
-        for (int x = 0; x < arrSplit.length; x++){
-            if (arrSplit[x] != null){
-                System.out.println(arrSplit[x]);
+        for (int a = 0; a < arrSplit.length; a++){
+            if (arrSplit[a] !=null){
+            sorted[a] = arrSplit[a];
             }
         }
         
+        removeElements(arrSplit);
         
-        Arrays.sort(arrSplit);
         
-        for (int c = 0; c < arrSplit.length; c++){
-            if (arrSplit[c] !=null){
-                for (int d= 0; d < arrSplit.length; d++)
-                    sorted[c] = (arrSplit[c]);
-            }
-        }
+        
+
+        
+        
         
     }
+    
+    public static String[] removeElements(String[] allElements) {
+    // 1 : count
+    int n = 0;
+    for (int o = 0; o < allElements.length; o++)
+        if (allElements[o] != null) n++;
+
+    // 2 : allocate new array
+    String[] _localAllElements = new String[n];
+
+    // 3 : copy not null elements
+    int p = 0;
+    for (int q = 0; q < allElements.length; q++)
+        if (allElements[q] != null)
+            _localAllElements[p++] = allElements[q];
+                    Arrays.sort(_localAllElements);
+                    System.out.println("sorted:");
+                    for (int s = 0; s < _localAllElements.length; s++){
+                        System.out.println(_localAllElements[s]);
+                    }
+
+
+    return _localAllElements;
+}
 }
