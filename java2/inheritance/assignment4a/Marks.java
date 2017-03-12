59e63592dae405df1ece2fe97d7d14fab0c05e03
ionakathryn <ionakathryn123@googlemail.com>
2017-03-12 18:01:13 +0000

    Decided to use StringTokenizer

--- java2/inheritance/assignment4a/Marks.java
+++ java2/inheritance/assignment4a/Marks.java
@@ -1,89 +1,44 @@
 import java.io.*;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Marks {
   public static void main(String[] args) {
 
     // method for opening file
     BufferedReader in = null;
-    String output = "";
-    String[] arr = new String[60];
-    String[] arrSplit = new String[60];
-    String[] sorted = new String[arrSplit.length];
-    int i = 0;
-    String delimiters = "[0-9]";
+    String line = "";
+    String arr[] = new String[60];
 
-    try { in = new BufferedReader(new FileReader("Marks.txt"));
+    try { 
+      in = new BufferedReader(new FileReader("Marks.txt"));
     } catch (FileNotFoundException e) {
       System.out.println("File Not Found!!");
       System.exit(0);
     }
 
-    while (output != null) {
+    while (line != null) {
       try {
-        output = in .readLine();
-        arr[i] = output;
-        i++;
+        StringTokenizer stk = new StringTokenizer(line);
+        line = in .readLine();
+      }
+      
 
-      } catch (IOException ioe) {
+      
+       catch (IOException ioe) {
         System.out.println("I/O error");
         System.exit(1);
       }
-    }
 
-    // split string when int is found
-    for (int j = 0; j < arr.length; j++) {
-      if (arr[j] != null) {
-        for (int k = 0; k < arr.length; k++) {
-          arrSplit[j] = (Arrays.toString(arr[j].split(delimiters)));
-        }
-
-      }
-
-    }
-
-    for (int a = 0; a < arrSplit.length; a++) {
-      if (arrSplit[a] != null) {
-        sorted[a] = arrSplit[a];
-      }
-    }
-
-    removeElements(arr);
-    for (int z = 0; z < arr.length; z++) {
-      if (arr[z] != null) {
-        for (int q = 0; q < arr.length; q++) {
-          sorted[q] = (Arrays.toString(arr[z].split(" ")));
+      for (int j = 0; j < arr.length ; j++){
+        if (arr[j]!=null){
+          System.out.println(arr[j]);
         }
       }
-    }
 
-  }
 
-  public static void removeElements(String[] allElements) {
-    // 1 : count
-    int n = 0;
-
-    for (int o = 0; o < allElements.length; o++) {
-      if (allElements[o] != null) {
-        n++;
-      }
-    }
-
-    // 2 : allocate new array
-    String[] localAllElements = new String[n];
-    int newIndex = 0;
-
-    // 3 : copy not null elements
-    for (int q = 0; q < allElements.length; q++) {
-      if (allElements[q] != null) {
-        localAllElements[newIndex] = allElements[q];
-        newIndex++;
-      }
+    
 
     }
-
-    Arrays.sort(localAllElements);
-    System.out.println(Arrays.toString(localAllElements));
-
   }
 }
