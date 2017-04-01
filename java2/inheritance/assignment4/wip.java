451f9487d9bfd7f089dd83a78929d5a292f8fc28
ionakathryn <ionakathryn123@googlemail.com>
2017-04-01 21:38:39 +0100

    Update wip.java

--- java2/inheritance/assignment4/wip.java
+++ java2/inheritance/assignment4/wip.java
@@ -1,7 +1,6 @@
 import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
-import java.io.IOException;
 
 public class Marks {
   public static void main(String[] args) {
@@ -10,10 +9,11 @@ public class Marks {
     BufferedReader in = null;
     String line = "";
     String[] name = new String[60];
+    String[] surName = new String[60];
     String[] marks = new String[60];
+
     String tempMark = "";
     int count = 0;
-    StringTokenizer stk = new StringTokenizer("");
 
     try { 
       in = new BufferedReader(new FileReader("Marks.txt"));
@@ -21,56 +21,61 @@ public class Marks {
       System.out.println("File Not Found!!");
       System.exit(0);
     }
-  
- String read = "";
 
-   try{
-    
-      while (read != null){
-      read = in.readLine();
-      stk = new StringTokenizer(read);
-    }
-  }
-  catch (IOException e){
-    System.out.println("I/O error");
-    System.exit(1);
-  }
-    
-          
-      
-
+    while (line != null) {
+      try {
+        StringTokenizer stk = new StringTokenizer(line);
+        line = in.readLine();
         while (stk.hasMoreTokens()){
-            name[count] = stk.nextToken() + " " + stk.nextToken();
+            name[count] = stk.nextToken();
+            surName[count] = " " + stk.nextToken();
             marks[count] = stk.nextToken();
             count ++;
+          }
+      }
+            
+       catch (IOException ioe) {
+        System.out.println("I/O error");
+        System.exit(1);
+      }
           }
 
       String temp = "";
       String print;
       print = String.format("\033[4m%4s\033[0m" + "\t            \033[4m%-5s\033[0m", "Name", "Marks");
       System.out.println(print);
-      
+
+      boolean swap = true;
+        while(swap)
+        {
+          swap = false;
           for (int i = 0; i < name.length-1; i++){
             for (int j = 0; j < name.length-1; j++){
               if ((name[i]!=null) && (name[i+1]!=null)){
-              if (name[i].compareTo(name[i+1]) > 0){
+              if (surName[i].compareTo(surName[i+1]) > 0){
                 temp = name[i];
                 tempMark = marks[i];
                 marks[i] = marks[i+1];
                 name[i] = name[i+1];
+                surName[i] = surName[i+1];
+                surName[i+1] = temp;
                 name[i+1] = temp;
                 marks[i+1] = tempMark;
-                }
-              }
-            }
+                swap = true;
+        }
+      }
+    }
+}
+              
+            
             
           }
           for (int a = 0; a < name.length; a++){
             if (name[a]!= null){
-            print = String.format("%4s\t    %-7s", name[a], marks[a]);
+            print = String.format("%4s\t %4s   %-7s", name[a], surName[a], marks[a]);
             System.out.println(print);
           }
         }
+        
+    }
   }
-  }
-  
