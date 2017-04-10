1a4cc9f6a57415ae36279eeb3d4792b01d46706f
ionakathryn <ionakathryn123@googlemail.com>
2017-04-10 20:39:29 +0100

    Update Tile.java

--- java2/inheritance/TileGame/Tile.java
+++ java2/inheritance/TileGame/Tile.java
@@ -2,8 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 import javax.swing.JButton;
 import java.awt.event.*;
-import java.awt.AWTEvent;
-
+import java.util.Random;
+import java.util.Arrays;
 
 public class Tile {
     public static void main (String[] args) {
@@ -19,31 +19,32 @@ class TileFrame extends JFrame implements ActionListener{
 
     public TileFrame(){
         Panel panel1,panel2;
-        JButton buttons[] = new JButton[16],blank;
         JFrame frame = new JFrame ("TileGame");
 
         final int DEFAULT_FRAME_WIDTH = 500;
         final int DEFAULT_FRAME_HEIGHT = 500;
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setLayout(new GridLayout(4,0));
+        setLayout(new GridLayout(4,4));
 
-        setSize(DEFAULT_FRAME_WIDTH,DEFAULT_FRAME_HEIGHT); 
-        JButton shuffleButton = new JButton("Shuffle");
+        setSize(DEFAULT_FRAME_WIDTH,DEFAULT_FRAME_HEIGHT);
+        shuffleButton = new JButton("Shuffle");
         panel1 = new Panel(new GridLayout(4,4));
         panel2 = new Panel(new GridLayout(2,2));
-        frame.setTitle("Tile Game");       
-        
+        frame.setTitle("Tile Game");
 
+        // add button arr to frame
         for (int i = 0; i < 16; i++){
             buttons[i] = new JButton();
             buttons[i].addActionListener(this);
             buttons[i].setText(Integer.toString(i+1));
             panel1.add(buttons[i]);
-           
+
         }
 
         blank = new JButton(" ");
+        blank.addActionListener(this);
         panel1.add(blank);
+        shuffleButton.addActionListener(this);
         panel2.add(shuffleButton);
         add(panel1);
         add(panel2);
@@ -51,20 +52,36 @@ class TileFrame extends JFrame implements ActionListener{
     }
 
     public void actionPerformed(ActionEvent e){
-        System.out.println("test");
+        if (e.getSource() == shuffleButton){
+            System.out.println("shuffletruffle");
+            Shuffle(buttons);
+            System.out.println(Arrays.toString(buttons));
+        }
 
+        if(e.getSource() == buttons[10]) {
+            System.out.println("test");
+        }
+
+    }
+
+    JButton shuffleButton,blank;
+    JButton[] buttons = new JButton[16];
+
+    public void Shuffle(JButton[] arr){
+        int numOfElements = arr.length-1;
+        for (int i = 0; i < numOfElements-1; i++){
+            int index = i + (int) (Math.random() * (numOfElements-2));
+            JButton temp = arr[index];
+            arr[i] = temp;
+        }
     }
 }
 
-/*public void Shuffle(ActionEvent e){
-    // implement fishers shuffle algorithm here
 
-}
-*/
+
+
+
 
 /*public class MouseAdapter implements MouseListener{
-
 }
 */
-
-
