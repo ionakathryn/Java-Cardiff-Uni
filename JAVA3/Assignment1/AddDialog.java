3e99a15c081d858b7699f1108b7d983364a57bc8
ionakathryn <ionakathryn123@googlemail.com>
2017-05-03 22:15:47 +0100

    Update AddDialog.java

--- JAVA3/Assignment1/AddDialog.java
+++ JAVA3/Assignment1/AddDialog.java
@@ -1,22 +1,21 @@
 import javax.swing.JFrame;
 import java.awt.Dialog;
-import javax.swing.*;
 import java.awt.*;
 
 class AddDialog extends Dialog {
 
-	public AddDialog(JFrame f){
-	super(f, true);
-	final int DEFAULT_DIALOG_WIDTH = 500;
-	final int DEFAULT_DIALOG_HEIGHT = 500;
-	setSize(DEFAULT_DIALOG_WIDTH, DEFAULT_DIALOG_HEIGHT);
-	module_code = new TextField("module_code goes here");
-	module_code.setBounds(400,400,50,20);
-	error = new TextField("error goes here");
-	error.setBounds(250,250,250,10);
-	add(module_code);
-	add(error);
-	}
+    public AddDialog(JFrame f){
+        super(f, true);
+        final int DEFAULT_DIALOG_WIDTH = 500;
+        final int DEFAULT_DIALOG_HEIGHT = 500;
+        setSize(DEFAULT_DIALOG_WIDTH, DEFAULT_DIALOG_HEIGHT);
+        module_code = new TextField("module_code");
+        module_code.setBounds(200,10,110,20);
+        error = new TextArea(10,10 );
+        add(module_code);
+        add(error);
+    }
 
-	private TextField module_code, error;
+    private TextField module_code;
+    private TextArea error;
 }
