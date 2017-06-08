12aeade8db8125babaa1e1ecf5de5ace00585430
ionakathryn <ionakathryn123@googlemail.com>
2017-06-08 19:09:50 +0100

    added functionality to write correct num of bytes to file

--- JAVA3/Assignment1/AddDialog.java
+++ JAVA3/Assignment1/AddDialog.java
@@ -8,8 +8,8 @@ import java.io.IOException;
 public class AddDialog extends JDialog {
 	public AddDialog(JFrame f, boolean b) {
 		super(f, true);
-		final int DEFAULT_DIALOG_WIDTH = 750;
-		final int DEFAULT_DIALOG_HEIGHT = 850;
+		final int DEFAULT_DIALOG_WIDTH = 900;
+		final int DEFAULT_DIALOG_HEIGHT = 800;
 		setSize(DEFAULT_DIALOG_WIDTH, DEFAULT_DIALOG_HEIGHT);
 		setTitle("addRecord");
 		module_code = new JTextField("module_code");
@@ -20,7 +20,7 @@ public class AddDialog extends JDialog {
 		surnameL.setSize(100,50);
 		surnameL.setLocation(50,80);
 		add(surnameL);
-		surname = new JTextField("surname");
+		surname = new JTextField(" ");
 		surname.setSize(100, 50);
 		surname.setLocation(200, 80);
 		add(surname);
@@ -28,7 +28,7 @@ public class AddDialog extends JDialog {
 		initialsL.setSize(100,50);
 		initialsL.setLocation(50,160);
 		add(initialsL);
-		initials = new JTextField("initials");
+		initials = new JTextField(" ");
 		initials.setSize(100, 50);
 		initials.setLocation(200, 160);
 		add(initials);
@@ -36,7 +36,7 @@ public class AddDialog extends JDialog {
 		titleL.setSize(100,50);
 		titleL.setLocation(50,240);
 		add(titleL);
-		title = new JTextField("title");
+		title = new JTextField(" ");
 		title.setSize(100, 50);
 		title.setLocation(200, 240);
 		add(title);
@@ -44,7 +44,7 @@ public class AddDialog extends JDialog {
 		studentNumL.setSize(100,50);
 		studentNumL.setLocation(50,320);
 		add(studentNumL);
-		studentNum = new JTextField("studentNum");
+		studentNum = new JTextField(" ");
 		studentNum.setSize(100, 50);
 		studentNum.setLocation(200, 320);
 		add(studentNum);
@@ -52,7 +52,7 @@ public class AddDialog extends JDialog {
 		assesment_markL.setSize(100,50);
 		assesment_markL.setLocation(50,400);
 		add(assesment_markL);
-		assesment_mark = new JTextField("assesment_mark");
+		assesment_mark = new JTextField(" ");
 		assesment_mark.setSize(100, 50);
 		assesment_mark.setLocation(200, 400);
 		add(assesment_mark);
@@ -60,7 +60,7 @@ public class AddDialog extends JDialog {
 		exam_markL.setSize(100,50);
 		exam_markL.setLocation(50,480);
 		add(exam_markL);
-		exam_mark = new JTextField("exam_mark");
+		exam_mark = new JTextField(" ");
 		exam_mark.setSize(100, 50);
 		exam_mark.setLocation(200, 480);
 		add(exam_mark);
@@ -68,7 +68,7 @@ public class AddDialog extends JDialog {
 		tuition_feeL.setSize(100,50);
 		tuition_feeL.setLocation(50,560);
 		add(tuition_feeL);
-		tuition_fee = new JTextField("tuition fee:");
+		tuition_fee = new JTextField(" ");
 		tuition_fee.setSize(100,50);
 		tuition_fee.setLocation(200,560);
 		add(tuition_fee);
@@ -79,11 +79,11 @@ public class AddDialog extends JDialog {
 		submit.setLocation(50,620);
 		submit.addActionListener(handle);
 		add(submit);
-		exit = new JButton("exit");
-		exit.setSize(100,50);
-		exit.setLocation(200,620);
-		exit.addActionListener(handle);
-		add(exit);
+		back = new JButton("back");
+		back.setSize(100,50);
+		back.setLocation(200,620);
+		back.addActionListener(handle);
+		add(back);
 		addUser = new JButton("add new user");
 		addUser.setSize(200,50);
 		addUser.setLocation(340,620);
@@ -97,14 +97,14 @@ public class AddDialog extends JDialog {
 	}
 	handler handle = new handler();
 	public JTextField error, module_code, surname, initials, title,studentNum, assesment_mark, exam_mark, tuition_fee ;
-	public JButton submit,exit, addUser;
+	public JButton submit,back, addUser;
 	public JLabel surnameL, initialsL,titleL,studentNumL,assesment_markL,exam_markL, tuition_feeL;
 	int count, i = 0;
 	int module_codeVal, studentNumVal, assesment_markVal, exam_markVal, tuition_feeVal, markVal;
 	String surnameVal, initialsVal, titleVal;
 	public class handler implements ActionListener {
 		public void actionPerformed(ActionEvent e){
-			if (e.getSource() == exit){
+			if (e.getSource() == back){
 				System.exit(0);
 			}
 			if(e.getSource() == addUser){
@@ -174,10 +174,10 @@ public class AddDialog extends JDialog {
 	}
 
 	RandomAccessFile file = null;
+	int recSize = 96;
 	public void makeNewFile(){
-		int recSize = 96;
 		try{
-			file = new RandomAccessFile("file.txt", "rw");
+			file = new RandomAccessFile(module_code.getText() + ".binary", "rw");
 			JOptionPane.showMessageDialog(null,"RandomAccessFile " + module_code.getText() + " created succesfully!!!");
 		}
 		catch (IOException ioe){
@@ -192,8 +192,11 @@ public class AddDialog extends JDialog {
 	}
 
 	public void write() throws IOException{
+		file.seek((Integer.parseInt(studentNum.getText())-1) * recSize);
+
 		// write module code
 		if (module_code.getText().length() < 6){
+			file.writeChars(module_code.getText());
 			for (i = module_code.getText().length(); i < 6; i++){
 				file.writeChar(' ');
 			}
@@ -205,6 +208,7 @@ public class AddDialog extends JDialog {
 
 		// write title
 		if (title.getText().length() < 6){
+			file.writeChars(title.getText());
 			for (i = title.getText().length(); i < 6; i++){
 				file.writeChar(' ');
 			}
@@ -216,6 +220,7 @@ public class AddDialog extends JDialog {
 
 		// write initials
 		if (initials.getText().length() < 6){
+			file.writeChars(initials.getText());
 			for (i = initials.getText().length(); i < 6; i++){
 				file.writeChar(' ');
 			}
@@ -227,6 +232,7 @@ public class AddDialog extends JDialog {
 
 		// write surname
 		if (surname.getText().length() < 20){
+			file.writeChars(surname.getText());
 			for (i = surname.getText().length(); i < 20; i++){
 				file.writeChar(' ');
 			}
@@ -235,7 +241,6 @@ public class AddDialog extends JDialog {
 				file.writeChars(surname.getText().substring(0,20));
 			}
 		
-
 		// write student num
 		file.writeInt(Integer.parseInt(studentNum.getText()));
 
